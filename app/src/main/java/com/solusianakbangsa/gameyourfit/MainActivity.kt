98732f8f8c4ffab27014c39db1283b8ede461b4c
moniquesenjaya <monique.senjaya@binus.ac.id>
2021-02-18 00:45:07 +0700

    Added Splashscreen to check if user is logged in

--- app/src/main/java/com/solusianakbangsa/gameyourfit/MainActivity.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.solusianakbangsa.gameyourfit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+
+        /**If user is authenticated, send them to dashboard, if not, send to login activity*/
+        Handler().postDelayed({
+            if(user!=null){
+                val dashboardIntent = Intent(this, DashboardActivity::class.java)
+                startActivity(dashboardIntent)
+                finish()
+            }else{
+                val loginIntent = Intent(this, LoginActivity::class.java)
+                startActivity(loginIntent)
+                finish()
+            }
+        }, 2000)
     }
 }
