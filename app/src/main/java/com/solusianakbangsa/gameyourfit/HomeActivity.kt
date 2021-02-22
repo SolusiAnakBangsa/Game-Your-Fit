3990e00626148d166c36e38e41f0db1e9f256bb7
Aric <hernandoaric@gmail.com>
2021-02-22 20:32:56 +0700

    changed design to fit more with mockup

--- app/src/main/java/com/solusianakbangsa/gameyourfit/HomeActivity.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/HomeActivity.kt
@@ -27,6 +27,7 @@ class HomeActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
