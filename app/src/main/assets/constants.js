fb89be28b1b240278a5ef6023a155e73b7207ad9
Aric <hernandoaric@gmail.com>
2021-03-05 01:35:10 +0700

    implemented leaderboard

--- app/src/main/assets/constants.js
+++ app/src/main/assets/constants.js
@@ -3,6 +3,6 @@
 // You can make the username and password from numb.viagenie.ca
 // Don't use your real password
 var ACCOUNT_INFO = {
-    username : "aric4121@gmail.com",
-    password : "Homonym23"
+    username : "Insert username info here",
+    password : "Insert password info here"
 }
