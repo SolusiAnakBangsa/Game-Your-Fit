31245b21cfb07aa5fb15ca0174baa3979372f8ef
Aric <hernandoaric@gmail.com>
2021-03-08 14:42:32 +0700

    changed kotlin compiler version, added json objects

---
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/json/Level.kt
@@ -0,0 +1,5 @@
+package com.solusianakbangsa.gameyourfit.json
+
+data class Level(val title : String, val taskList : List<Task> ,val thumbnail : String = "defaultLink???") {
+
+}
---
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/json/Task.kt
@@ -0,0 +1,3 @@
+package com.solusianakbangsa.gameyourfit.json
+
+data class Task(val frequency : Int, val name : String)
