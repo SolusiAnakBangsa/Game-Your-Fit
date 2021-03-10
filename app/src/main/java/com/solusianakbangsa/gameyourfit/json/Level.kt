822f5a4f96ac602179628aceadc4cda3c83f7dd4
Aric <hernandoaric@gmail.com>
2021-03-10 15:43:23 +0700

    changed id for drawer profile

--- app/src/main/java/com/solusianakbangsa/gameyourfit/json/Level.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/json/Level.kt
@@ -1,3 +1,3 @@
 package com.solusianakbangsa.gameyourfit.json
 
-data class Level(val title : String, val taskList : String ,val thumbnail : String = "defaultLink???")
+data class Level(val title : String, val tasks : String ,val thumbnail : String = "defaultLink???")
--- app/src/main/java/com/solusianakbangsa/gameyourfit/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/ui/dashboard/DashboardFragment.kt
@@ -1,7 +1,10 @@
 package com.solusianakbangsa.gameyourfit.ui.dashboard
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,12 +15,16 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.solusianakbangsa.gameyourfit.*
 import com.solusianakbangsa.gameyourfit.databinding.FragmentDashboardBinding
+import com.solusianakbangsa.gameyourfit.ui.ImageReplacer.replaceImage
+import java.net.URL
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var handler : Handler
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -26,7 +33,18 @@ class DashboardFragment : Fragment() {
     ): View? {
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        handler = Handler(Looper.getMainLooper())
         dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        replaceImage(
+            handler,
+            binding.cardProfilePicture,
+            "https://i.kym-cdn.com/entries/icons/original/000/036/482/cover5.jpg"
+        )
+//        var url = URL("https://i.kym-cdn.com/entries/icons/original/000/036/482/cover5.jpg").openConnection().getInputStream()
+//        var bmp = BitmapFactory.decodeStream(url)
+//
+//        binding.cardProfilePicture.setImageBitmap(bmp)
 
         binding.cardProfilePicture.setOnClickListener {
             val intent = Intent (activity, ProfileActivity::class.java)
