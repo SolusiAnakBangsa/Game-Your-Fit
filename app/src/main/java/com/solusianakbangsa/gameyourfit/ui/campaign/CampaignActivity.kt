f25be5af2f5472968591e1c41153c834afd4b6e2
Aric <hernandoaric@gmail.com>
2021-03-11 23:35:38 +0700

    added dynamic layout for levels

--- app/src/main/java/com/solusianakbangsa/gameyourfit/ui/campaign/CampaignActivity.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/ui/campaign/CampaignActivity.kt
@@ -3,11 +3,14 @@ package com.solusianakbangsa.gameyourfit.ui.campaign
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import com.solusianakbangsa.gameyourfit.AlphaOneActivity
 import com.solusianakbangsa.gameyourfit.JsonConstants
@@ -22,6 +25,7 @@ import java.net.URL
 class CampaignActivity : AppCompatActivity() {
     private lateinit var levelList : LevelList
     private lateinit var  levelContainer : LinearLayout
+    private val handler : Handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_campaign)
@@ -37,20 +41,20 @@ class CampaignActivity : AppCompatActivity() {
             Log.i("json","LEVEL DOENS't EXIST")
         }
 
-//        initializeLevels()
+        initializeLevels()
 
-        var levelButton0 :ImageView= findViewById(R.id.levelButton0)
-        levelButton0.setOnClickListener{
-            val intent = Intent(this, AlphaOneActivity::class.java)
-            intent.putExtra("taskList", levelList.getTasksAtLevel(0).toString())
-            this.startActivity(intent)
-        }
-        var levelButton1 :ImageView= findViewById(R.id.levelButton1)
-        levelButton1.setOnClickListener{
-            val intent = Intent(this, AlphaOneActivity::class.java)
-            intent.putExtra("taskList", levelList.getTasksAtLevel(1).toString())
-            this.startActivity(intent)
-        }
+//        var levelButton0 :ImageView= findViewById(R.id.levelButton0)
+//        levelButton0.setOnClickListener{
+//            val intent = Intent(this, AlphaOneActivity::class.java)
+//            intent.putExtra("taskList", levelList.getTasksAtLevel(0).toString())
+//            this.startActivity(intent)
+//        }
+//        var levelButton1 :ImageView= findViewById(R.id.levelButton1)
+//        levelButton1.setOnClickListener{
+//            val intent = Intent(this, AlphaOneActivity::class.java)
+//            intent.putExtra("taskList", levelList.getTasksAtLevel(1).toString())
+//            this.startActivity(intent)
+//        }
     }
 
     fun initializeLevels(){
@@ -58,15 +62,19 @@ class CampaignActivity : AppCompatActivity() {
 
         for (i in 0 until levelList.jsonArr.length()){
 //            TODO : putString of taskList during intent
-//            var levelView : View = layoutInflater.inflate(R.layout.x, null, false)
-//            var levelButton :ImageView = levelView.findViewById(R.id.levelThumbnail)
-//            replaceImage()
+            var levelView : View = layoutInflater.inflate(R.layout.level_card, null, false)
+            var levelButton :ImageView = levelView.findViewById(R.id.levelButton)
+            var levelTitle : TextView = levelView.findViewById(R.id.levelName)
+            levelTitle.text = levelList.getTitleAtLevel(i)
+            replaceImage(handler, levelButton, levelList.getThumbnailAtLevel(i))
 
-//            levelButton.setOnClickListener{
-//                val intent = Intent(this, CampaignActivity::class.java)
-//                intent.putExtra("TaskList", levelList.getTasksAtLevel(i).toString())
-//                this?.startActivity(intent)
-//            }
+
+            levelButton.setOnClickListener{
+                val intent = Intent(this, AlphaOneActivity::class.java)
+                intent.putExtra("TaskList", levelList.getTasksAtLevel(i).toString())
+                this?.startActivity(intent)
+            }
+            levelContainer.addView(levelView)
         }
     }
 
