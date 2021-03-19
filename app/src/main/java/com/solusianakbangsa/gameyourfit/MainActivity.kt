b5a7729d4943ce4ee1f12bc308d1feefd0a975f9
moniquesenjaya <monique.senjaya@binus.ac.id>
2021-03-19 12:50:47 +0700

    made user profiling check

--- app/src/main/java/com/solusianakbangsa/gameyourfit/MainActivity.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.solusianakbangsa.gameyourfit
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.solusianakbangsa.gameyourfit.ui.auth.LoginActivity
 
 class MainActivity : AppCompatActivity() {
@@ -17,13 +23,30 @@ class MainActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
         val user = mAuth.currentUser
+        val dbRef = FirebaseDatabase.getInstance().reference.child("users").child(user.uid.toString())
 
         /**If user is authenticated, send them to dashboard, if not, send to login activity*/
         Handler().postDelayed({
             if(user!=null){
-                val dashboardIntent = Intent(this, HomeActivity::class.java)
-                startActivity(dashboardIntent)
-                finish()
+                dbRef.addListenerForSingleValueEvent(object: ValueEventListener{
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.i("Error", "Could not connect to Database")
+                    }
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.hasChild("userHeight")){
+                            val dashboardIntent = Intent(this@MainActivity, HomeActivity::class.java)
+                            toast("Please fill in your details.")
+                            startActivity(dashboardIntent)
+                            finish()
+                        }else{
+                            val dashboardIntent = Intent(this@MainActivity, ProfileActivity::class.java)
+                            startActivity(dashboardIntent)
+                            finish()
+                        }
+                    }
+                })
+
             }else{
                 val loginIntent = Intent(this, LoginActivity::class.java)
                 startActivity(loginIntent)
