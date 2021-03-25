57dafad7e5d5168fa7385d44bb87502d7da25141
moniquesenjaya <monique.senjaya@binus.ac.id>
2021-03-25 16:36:12 +0700

    Added shared preference initializer reader

--- app/src/main/java/com/solusianakbangsa/gameyourfit/HomeActivity.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.Menu
 import android.view.View
 import android.widget.ImageView
@@ -24,7 +25,7 @@ import androidx.preference.PreferenceManager
 import com.firebase.ui.auth.AuthUI
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.solusianakbangsa.gameyourfit.ui.ImageReplacer
 import com.solusianakbangsa.gameyourfit.ui.auth.LoginActivity
 import kotlinx.android.synthetic.main.nav_header_main.*
@@ -35,6 +36,7 @@ class HomeActivity : AppCompatActivity() {
     lateinit var sharedPref: SharedPreferences
     private lateinit var appBarConfiguration: AppBarConfiguration
     private val imageReplacer = ImageReplacer()
+    lateinit var ref: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -60,7 +62,44 @@ class HomeActivity : AppCompatActivity() {
 
         val userId = FirebaseAuth.getInstance().uid.toString()
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
-        val ref = FirebaseDatabase.getInstance().getReference("users").child(userId)
+        ref = FirebaseDatabase.getInstance().getReference("users").child(userId)
+
+        ref.addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {}
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.i("helpme", sharedPref.all.toString())
+                if (snapshot.child("username").exists()){
+                    sharedPref.edit().putString("username", snapshot.child("username").value.toString()).apply()
+                    navView.getHeaderView(0).findViewById<TextView>(R.id.drawerName).text = sharedPref.getString("username", "").toString()
+                    Log.i("helpme", sharedPref.all.toString())
+                }
+                if (snapshot.child("image").exists()){
+                    sharedPref.edit().putString("image", snapshot.child("image").value.toString()).apply()
+                    imageReplacer.replaceImage(
+                        handler,
+                        drawerProfilePicture,
+                        sharedPref.getString("image", "").toString(),
+                        null,
+                        this@HomeActivity,
+                        FileConstants.PROFILE_PICTURE_FILENAME
+                    )
+                }
+                if (snapshot.child("userWeight").exists()){
+                    sharedPref.edit().putLong("userWeight", snapshot.child("userWeight").value.toString().toLong()).apply()
+                }
+                if (snapshot.child("userHeight").exists()){
+                    sharedPref.edit().putLong("userHeight", snapshot.child("userHeight").value.toString().toLong()).apply()
+                }
+                if (snapshot.child("level").exists()){
+                    sharedPref.edit().putInt("level", snapshot.child("level").value.toString().toInt()).apply()
+                }
+                if (snapshot.child("exp").exists()){
+                    sharedPref.edit().putLong("exp", snapshot.child("exp").value.toString().toLong()).apply()
+                }
+            }
+        })
+
+        Log.i("helpme", sharedPref.all.toString())
         var name: String = "Username"
 
         if (sharedPref.contains("username")) {
@@ -99,6 +138,7 @@ class HomeActivity : AppCompatActivity() {
 
         val logout = navView.getHeaderView(0).findViewById<TextView>(R.id.logout)
         logout.setOnClickListener {
+            Log.i("popup", "listenercool")
             MaterialAlertDialogBuilder(this, R.style.AlertDialogTheme)
                 .setTitle("Logout")
                 .setMessage("Are you sure you want to logout of this account?")
@@ -115,10 +155,15 @@ class HomeActivity : AppCompatActivity() {
                         finish()
                     }
                 }
+                .show()
         }
     }
 
-        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    private fun dataInit() {
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
 //      Disables option menu (the one with 3 dots)
             return true
         }
