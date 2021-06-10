decf14eebdcb33eb33ad18507a16936991a57717
Jason Christian H <nakamarujc@gmail.com>
2021-06-10 18:49:32 +0700

    Added countdown visuals

--- app/src/main/java/com/solusianakbangsa/gameyourfit/cam/game/GameOverlay.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/cam/game/GameOverlay.kt
@@ -31,21 +31,49 @@ class GameOverlay(context: Context?, attrs: AttributeSet?) : View(context, attrs
     private val spawnWidthBorder = 0.2f
     private val spawnHeightBorder = 0.3f
 
-    private var isInstructionDone : Boolean = false
-    private var isGameStarted = false
     private var isGenNewDot = true
-    private var isTimerStarted = false
+
+    private var gameState = GameState.INSTR
 
     private val startTimer = Timer()
     private var startTaskTimer : TimerTask? = null
 
     private val rotatePhoneIcon : Bitmap
 
-    var leftHand = PointF()
-    var rightHand = PointF()
+    private var leftHand = PointF()
+    private var rightHand = PointF()
+
+    private var showMiddleText = false
+    private var paintMiddleText = Paint()
+    private var paintMiddleTextS = Paint()
+    private var middleText = ""
+    set(text) {
+        showMiddleText = (text.isNotEmpty())
+        field = text
+    }
+
+    private enum class GameState {
+        INSTR,      // Instruction
+        STANDBY,    // Standby and wait until user gets in position
+        WAIT,       // User is in camera, and countdown started
+        START       // Game is started
+    }
 
     init {
         targetPaint.color = Color.BLUE
+
+        paintMiddleText.apply {
+            color = Color.WHITE
+            typeface = Typeface.DEFAULT_BOLD
+            textAlign = Paint.Align.CENTER
+        }
+
+        paintMiddleTextS.set(paintMiddleText)
+        paintMiddleTextS.apply {
+            style = Paint.Style.STROKE
+            color = Color.BLACK
+        }
+
         rotatePhoneIcon = getBitmapDrawable(
             context?.resources!!,
             R.drawable.ic_baseline_screen_rotation_24
@@ -53,7 +81,7 @@ class GameOverlay(context: Context?, attrs: AttributeSet?) : View(context, attrs
     }
 
     fun instructionDone() {
-        isInstructionDone = true
+        gameState = GameState.STANDBY
         generateNewTarget()
     }
 
@@ -62,12 +90,9 @@ class GameOverlay(context: Context?, attrs: AttributeSet?) : View(context, attrs
 
         val landmarks = pose?.allPoseLandmarks
         if (landmarks != null && landmarks.isNotEmpty()) {
-            // Get all the points
-            leftHand = getPointSum(landmarks, L_LEFT_HAND)
-            rightHand = getPointSum(landmarks, L_RIGHT_HAND)
 
             // Check if the player is in frame
-            if (isInstructionDone && !isGameStarted) {
+            if (gameState == GameState.STANDBY || gameState == GameState.WAIT) {
                 val upper = getPointSum(landmarks, L_SHOULDERS)
                 val bottom = getPointSum(landmarks, L_BOTTOM)
 
@@ -77,40 +102,67 @@ class GameOverlay(context: Context?, attrs: AttributeSet?) : View(context, attrs
                     isLandmarkInScreen(landmarks[23])) {
                     // If the user is inside the screen
 
-                    if (!isTimerStarted) {
-                        isTimerStarted = true
+                    if (gameState == GameState.STANDBY) {
+                        gameState = GameState.WAIT
                         startTaskTimer = object : TimerTask() {
                             override fun run() {
-                                isGameStarted = true // TODO: Change with function
+                                gameState = GameState.START // TODO: Change with function
                                 Log.i("Bruh", "Bruh lol")
                             }
                         }
-                        startTimer.schedule(startTaskTimer, 5000L)
+                        startTimer.schedule(startTaskTimer, 7000L)
                     }
-                } else if (isTimerStarted) {
+                } else if (gameState == GameState.WAIT || gameState == GameState.STANDBY) {
                     startTaskTimer?.cancel()
-                    isTimerStarted = false
+                    middleText = ""
+                    gameState = GameState.STANDBY
                 }
             }
-        }
 
-        Log.i("BruhBruh", isGameStarted.toString() + " " + isTimerStarted.toString() + " " + startTaskTimer?.scheduledExecutionTime().toString())
+            when (gameState) {
+                GameState.INSTR -> {
 
-        if (isGenNewDot) {
-            generateNewTarget()
-            isGenNewDot = false
-        } else {
-            if (((leftHand.x-targetCircle.x).pow(2) +
+                }
+                GameState.STANDBY -> {
+
+                }
+                GameState.WAIT -> {
+                    // Count time remaining
+                    val goTime = startTaskTimer!!.scheduledExecutionTime()
+                    val timeLeft = ((goTime - System.currentTimeMillis()) / 1000) - 1
+                    middleText = if (timeLeft > 0) {
+                        timeLeft.toString()
+                    } else {
+                        "Go!"
+                    }
+
+                }
+                GameState.START -> {
+                    // Position the hand
+                    leftHand = getPointSum(landmarks, L_LEFT_HAND)
+                    rightHand = getPointSum(landmarks, L_RIGHT_HAND)
+
+                    // Generate new dot
+                    if (isGenNewDot) {
+                        generateNewTarget()
+                        isGenNewDot = false
+                    } else if (((leftHand.x-targetCircle.x).pow(2) +
                             (leftHand.y-targetCircle.y).pow(2) < 150f.pow(2)) ||
-                    ((rightHand.x-targetCircle.x).pow(2) +
+                            ((rightHand.x-targetCircle.x).pow(2) +
                             (rightHand.y-targetCircle.y).pow(2) < 150f.pow(2))) {
-                isGenNewDot = true
+                        isGenNewDot = true
+                    }
+                }
             }
+
         }
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         //set the center of all circles to be center of the view
+        paintMiddleText.textSize = w.toFloat()/3
+        paintMiddleTextS.textSize = w.toFloat()/3
+        paintMiddleTextS.strokeWidth = w.toFloat()/25
     }
 
     override fun onAttachedToWindow() {
@@ -119,7 +171,6 @@ class GameOverlay(context: Context?, attrs: AttributeSet?) : View(context, attrs
         // Register ticker
         loopTicker = ValueAnimator.ofFloat(0f, 1f).apply {
             addUpdateListener {
-//                xlol = it.animatedValue as Float * 700f
                 onLoop()
                 postInvalidateOnAnimation()
             }
@@ -144,12 +195,29 @@ class GameOverlay(context: Context?, attrs: AttributeSet?) : View(context, attrs
 //            (height / 2 - rotatePhoneIcon.height / 2).toFloat(), targetPaint
 //        )
 
-        if (!isInstructionDone) return
+        when (gameState) {
+            GameState.INSTR -> {
 
-        canvas.drawCircle(targetCircle.x, targetCircle.y, 100f, targetPaint)
+            }
+            GameState.STANDBY -> {
 
-        drawPoint(canvas, leftHand, targetPaint)
-        drawPoint(canvas, rightHand, targetPaint)
+            }
+            GameState.WAIT -> {
+                // Count time remaining
+                if (showMiddleText) {
+                    val textX = width.toFloat()/2
+                    val textY = height.toFloat()/2 + paintMiddleText.textSize/3
+                    canvas.drawText(middleText, textX, textY, paintMiddleTextS)
+                    canvas.drawText(middleText, textX, textY, paintMiddleText)
+                }
+            }
+            GameState.START -> {
+                canvas.drawCircle(targetCircle.x, targetCircle.y, 100f, targetPaint)
+
+                drawPoint(canvas, leftHand, targetPaint)
+                drawPoint(canvas, rightHand, targetPaint)
+            }
+        }
     }
 
     private fun generateNewTarget() {
@@ -175,7 +243,7 @@ class GameOverlay(context: Context?, attrs: AttributeSet?) : View(context, attrs
 
     private fun translateX(x: Float): Float {
         return if (GameOverlay.overlay.isImageFlipped) {
-            GameOverlay.overlay.getWidth() - (scale(x) - GameOverlay.overlay.postScaleWidthOffset)
+            GameOverlay.overlay.width - (scale(x) - GameOverlay.overlay.postScaleWidthOffset)
         } else {
             scale(x) - GameOverlay.overlay.postScaleWidthOffset
         }
