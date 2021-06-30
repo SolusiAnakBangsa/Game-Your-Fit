48cf57be1c0df13b046a4f483597fa2b852dfc09
Aric <hernandoaric@gmail.com>
2021-06-30 15:12:58 +0700

    added nullcheck to playtime

--- app/src/main/java/com/solusianakbangsa/gameyourfit/util/FirebaseHelper.kt
+++ app/src/main/java/com/solusianakbangsa/gameyourfit/util/FirebaseHelper.kt
@@ -95,6 +95,12 @@ class FirebaseHelper {
             val userRef = buildFirebaseRef("users", getCurrentUID())
             userRef.addListenerForSingleValueEvent(object : ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot) {
+                    if(!snapshot.hasChild("totalPlaytime")){
+                        userRef.child("totalPlaytime").setValue(0)
+                    }
+                    if(!snapshot.hasChild("streakPlaytimeMillis")){
+                        userRef.child("streakPlaytimeMillis").setValue(0)
+                    }
                     userRef.child("totalPlaytime").setValue(snapshot.child("totalPlaytime").value as Long + totalMillis)
                     userRef.child("streakPlaytimeMillis").setValue(totalMillis)
                 }
